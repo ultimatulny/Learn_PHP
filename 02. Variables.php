64f0df2865df89b38a0bd2926e7063944317f534
Alexander <31167599+ultimatulny@users.noreply.github.com>
2018-11-30 19:38:41 +0500

    Create 02. Variables.php

---
+++ 02. Variables.php
@@ -0,0 +1,16 @@
+<?php 
+	
+	$num = 25;
+	$float = 0.125;
+	$string = "Hello!";
+	$bool = true;
+
+	echo "Num = $num";
+	echo "<br>";
+	echo "Float = $float";
+	echo "<br>";
+	echo "String = $string";
+	echo "<br>";
+	echo "Bool = $bool";
+	echo "<br>";
+ ?>
