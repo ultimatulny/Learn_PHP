8932e942e418842d40caf51015638b0555099e1a
Alexander <31167599+ultimatulny@users.noreply.github.com>
2018-12-01 22:17:28 +0500

    Create 03. Arifmetic operations.php

---
+++ 03. Arifmetic operations.php
@@ -0,0 +1,31 @@
+<?php 
+	
+	echo "<b>Arifmetic operations:</b>";
+	echo "<br><br>";
+	$a = 150;
+	$b = 40;
+
+	$c = $a + $b;
+	echo "Сумма $a & $b = $c";
+
+	echo "<br>";
+
+	$c = $a - $b;
+	echo "Разность $a & $b = $c";
+
+	echo "<br>";
+
+	$c = $a * $b;
+	echo "Произведение $a & $b = $c";
+
+	echo "<br>";
+
+	$c = $a / $b;
+	echo "Частное $a & $b = $c";
+	
+	echo "<br><br>";
+
+	$a++; 
+	echo "$a";
+	
+ ?>
