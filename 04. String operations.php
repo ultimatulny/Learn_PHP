ea8d8766cd8ffec770871f58179c7df3f74028a9
Alexander <31167599+ultimatulny@users.noreply.github.com>
2018-12-03 17:26:46 +0500

    Create 04. String operations.php

---
+++ 04. String operations.php
@@ -0,0 +1,12 @@
+<?php 
+	
+	echo "<b>String operations:</b> <br>";
+
+	$str_1 = "Hello";
+	$str_2 = "Welcome!";
+
+	echo $str_1." ".$str_2; // Объединение двух строк
+	echo "<br>";
+	echo " \" "; // Вывод символов в кавычках
+	
+ ?>
