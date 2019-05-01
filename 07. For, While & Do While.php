dc5ef72ab29ba25202c4f7e0e5bf2baff829c90f
Alexander <31167599+ultimatulny@users.noreply.github.com>
2019-05-01 18:18:25 +0500

    Create 07. For, While & Do While.php

---
+++ 07. For, While & Do While.php
@@ -0,0 +1,26 @@
+<?php 
+	
+	/*****
+	*
+	*	For, While & Do While
+	*
+	*****/
+
+	for ($i = 0; $i < 10; $i++) {
+		echo "$i<br>";
+	}
+
+	$x = 0;
+
+	while ($x < 10) {
+		echo "$x<br>";
+		$x++;
+	}
+
+	do{
+		echo "$x<br>";
+		$x--;
+	}while ($x > 0);
+
+
+ ?>
