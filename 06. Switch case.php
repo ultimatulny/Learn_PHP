158158b183bdc63f5c7c727e9243a99d64bc7b32
Alexander <31167599+ultimatulny@users.noreply.github.com>
2019-05-01 17:51:11 +0500

    Create 06. Switch case.php

---
+++ 06. Switch case.php
@@ -0,0 +1,31 @@
+<?php 
+	
+	/*****
+	*
+	*	Switch () case
+	*
+	*****/
+
+	$x = 2;
+	
+	switch ($x) {
+		case 1:
+			echo "X = 1";
+			break;
+
+		case 2:
+			echo "X = 2";
+			break;
+
+		case 3:
+			echo "X = 3";
+			break;
+		
+		default:
+			echo "Error :(";
+			break;
+	}
+
+
+
+ ?>
